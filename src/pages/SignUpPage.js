e1ffb1d04e947215540a62ce7169d5fcc5eba5c7
eldhosevj <eldhosvj@gmail.com>
2022-07-22 01:50:28 -0700

    second day

--- src/pages/SignUpPage.js
+++ src/pages/SignUpPage.js
@@ -1,56 +1,92 @@
 import axios from "axios";
-import React,{useState,useEffect} from "react";
+import React, { useState, useEffect } from "react";
 const SignUpPage = () => {
-const [value,setValues]=useState({
-    password:"",
-    repeatPassword:null,
-    email:"",
-    username:""
-})    
+  const [value, setValues] = useState({
+    password: "",
+    repeatPassword: null,
+    email: "",
+    username: "",
+  });
 
-//const [didUserEnterValue,setDidUserEnterValue]=useState(false)
-const onChangeHandler=(e)=>{
-setValues({...value,[e.target.name]:e.target.value})
-//setDidUserEnterValue(true)
-//setButtonDisabled(e.target.value!==value.repeatPassword)
-}
 
-// useEffect(()=>{
-// if(didUserEnterValue&&value.repeatPassword===value.password){
-//   setButtonDisabledButton(false)
-// }
-// },[value,didUserEnterValue])
-const submit=(event)=>{
-  event.preventDefault()
- // setButtonDisabledButton(true)
- const body=value
-  axios.post("/api/1.0/users",body)
-// fetch("/api/1.0/users",{
-//   method:"POST",
-//   headers:{
-//     "Content-Type":"application/json",
-//   },
-//   body:JSON.stringify(body)
-// })
-}
+  const onChangeHandler = (e) => {
+    setValues({ ...value, [e.target.name]: e.target.value });
+
+  };
+const [onGoingApiCall,setOnGoingApiCall]=useState(false)
+const [apiCallSuccesfull,setApiCallSucesful]=useState(false)
+const [error,setError]=useState({})
+  const submit = (event) => {
+    event.preventDefault();
+    setOnGoingApiCall(true)
+    const body = value;
+    axios.post("/api/1.0/users", body).then(()=>{
+      setOnGoingApiCall(false)
+      setApiCallSucesful(true)
+    }).catch((error)=>{
+     setOnGoingApiCall(false)
+     setApiCallSucesful(false)
+     setError(error.response.data.validationErrors)
+ 
+    });
+    // fetch("/api/1.0/users",{
+    //   method:"POST",
+    //   headers:{
+    //     "Content-Type":"application/json",
+    //   },
+    //   body:JSON.stringify(body)
+    // })
+  };
   return (
-    <div style={{width:"10%"}}>
+    <div style={{ width: "10%" }}>
       <h1>Sign Up</h1>
-    <form autoComplete="on">
-    <label htmlFor="user-name">User Name</label>
-      <input id="user-name" value={value.username} name="username"  onChange={onChangeHandler}/>
-      <label htmlFor="email">email</label>
+      {apiCallSuccesfull==false &&
+      <form autoComplete="on" data-testid="form-sign-up">
+        <label htmlFor="user-name">User Name</label>
+        <input
+          id="user-name"
+          value={value.username}
+          name="username"
+          onChange={onChangeHandler}
+        />
+        <p>{error.username}</p>
+        <label htmlFor="email">email</label>
 
-      <input id="email" value={value.email} onChange={onChangeHandler} name="email"/>
-      <label htmlFor="password">password</label>
+        <input
+          id="email"
+          value={value.email}
+          onChange={onChangeHandler}
+          name="email"
+        />
+        <label htmlFor="password">password</label>
 
-      <input id="password" name="password" onChange={onChangeHandler} value={value.password} type="password"/>
-      <label htmlFor="password-repeat"> password Repeat</label>
+        <input
+          id="password"
+          name="password"
+          onChange={onChangeHandler}
+          value={value.password}
+          type="password"
+        />
+        <label htmlFor="password-repeat"> password Repeat</label>
 
-      <input id="password-repeat"  name="repeatPassword" onChange={onChangeHandler} value={value.repeatPassword} type="password"/>
-      <button disabled={value.repeatPassword!==value.password} onClick={submit}>Sign UP</button>
-    </form>
+        <input
+          id="password-repeat"
+          name="repeatPassword"
+          onChange={onChangeHandler}
+          value={value.repeatPassword}
+          type="password"
+        />
+        <button
+          disabled={value.repeatPassword !== value.password||onGoingApiCall}
+          onClick={submit}
+        >
+          Sign UP
+        </button>
+      </form>
+      }
+      <p>{apiCallSuccesfull&&"Please check your Email to activate your account"}</p>
     </div>
   );
 };
+
 export default SignUpPage;
--- src/pages/SignUpPage.test.js
+++ src/pages/SignUpPage.test.js
@@ -1,4 +1,9 @@
-import { render, screen } from "@testing-library/react";
+import {
+  render,
+  screen,
+  waitFor,
+  waitForElementToBeRemoved,
+} from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import SignUpPage from "./SignUpPage";
 import axios from "axios";
@@ -63,43 +68,58 @@ describe("Sign UP", () => {
   });
 
   describe("Interactions", () => {
+    let button;
+    let requestBody;
+    let counter = 0;
+    const server = setupServer(
+      rest.post("/api/1.0/users", (req, res, ctx) => {
+        requestBody = req.body;
+        counter++;
+        return res(ctx.status(200));
+      })
+    );
+    beforeAll(() => {
+      server.listen();
+    });
+    beforeEach(() => {
+      counter = 0;
+      server.resetHandlers()
+      //server.resetHanlers is used to avoid overriding property for multiple text cases
+    });
+    afterAll(() => {
+      server.close();
+    });
+
+    let msg = "Please check your Email to activate your account";
+    const setUp = () => {
+      render(<SignUpPage />);
+      const password = screen.getByLabelText("password");
+      const passwordRepeat = screen.getByLabelText("password Repeat");
+      const email = screen.getByLabelText("email");
+      const userName = screen.getByLabelText("User Name");
+      button = screen.queryByRole("button", { name: "Sign UP" });
+      userEvent.type(password, "eddy");
+      userEvent.type(passwordRepeat, "eddy");
+      userEvent.type(email, "eldhosvj@gmail.com");
+      userEvent.type(userName, "eldhose");
+    };
     it("enable the button when password and password repeat has same value", () => {
       render(<SignUpPage />);
       const password = screen.getByLabelText("password");
       const passwordRepeat = screen.getByLabelText("password Repeat");
       userEvent.type(password, "a");
       userEvent.type(passwordRepeat, "a");
-      const button = screen.findBy;
+      const button = screen.queryByRole("button", { name: "Sign UP" });
       expect(button).toBeEnabled();
     });
-   
 
     it("send username,email and password after clicking the button", async () => {
-      let requestBody;
-      const server = setupServer(
-        rest.post("/api/1.0/users", (req, res, ctx) => {
-          requestBody = req.body;
-      
-          return res(ctx.status(200));
-        })
-      );
-
       // Establish requests interception layer before all tests.
-      server.listen();
-
-      render(<SignUpPage />);
-      const password = screen.getByLabelText("password");
-      const passwordRepeat = screen.getByLabelText("password Repeat");
-      const email = screen.getByLabelText("email");
-      const userName = screen.getByLabelText("User Name");
-      const button = screen.queryByRole("button", { name: "Sign UP" });
-      userEvent.type(password, "eddy");
-      userEvent.type(passwordRepeat, "eddy");
-      userEvent.type(email, "eldhosvj@gmail.com");
-      userEvent.type(userName, "eldhose");
+      setUp();
       userEvent.click(button);
-      await new Promise((resolve) => setTimeout(resolve, 500));
-// can you plese let me know why this requestBody undefined
+
+      //await new Promise((resolve) => setTimeout(resolve, 500));
+      await screen.findByText(msg);
       expect(requestBody).toEqual({
         password: "eddy",
         repeatPassword: "eddy",
@@ -107,8 +127,63 @@ describe("Sign UP", () => {
         username: "eldhose",
       });
     });
+
+    it("disable button when there is an ongoing api call", async () => {
+      // Establish requests interception layer before all tests.
+      server.listen();
+      setUp();
+      userEvent.click(button);
+      userEvent.click(button);
+      userEvent.click(button);
+
+      //await new Promise((resolve) => setTimeout(resolve, 500));
+
+      await screen.findByText(msg);
+
+      expect(counter).toBe(1);
+    });
+    it("display validation message for username", async() => {
+      server.use(
+        rest.post("/api/1.0/users", (req, res, ctx) => {
+          requestBody = req.body;
+          counter++;
+          // server overiding can be don using server.resetHandlers() or  res.once
+         // return res.once(ctx.status(400)
+          return res(ctx.status(400)
+          
+          ,
+          ctx.json(
+            {validationErrors:{
+              username: "Username cannot be null"
+            }}
+          ));
+        })
+      )
+      setUp();
+      userEvent.click(button)
+       let validationErrors=await screen.findByText("Username cannot be null")
+       expect(validationErrors).toBeInTheDocument()
+    });
+    it("hide signup form after succesful sign up request", async () => {
+      setUp();
+      const form = screen.getByTestId("form-sign-up");
+
+      userEvent.click(button);
+      /*
+one way of testing    
+await screen.findByText(msg)
+        expect(form).not.toBeInTheDocument();
   
-  
-  
+  */
+      //another way of testing using waitFor
+      // await waitFor(() => {
+      //   expect(form).not.toBeInTheDocument();
+      // });
+
+      //another way of testing
+      await waitForElementToBeRemoved(form);
+    });
+
+    
   });
 });
